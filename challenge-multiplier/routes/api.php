714129392eabb86f47034963541860a21a0c7bf6
Jallyson Rodrigues <43890752+jallysondev@users.noreply.github.com>
2023-10-27 23:06:27 -0300

    feat: create routes

--- challenge-multiplier/routes/api.php
+++ challenge-multiplier/routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Customer\CustomerController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(CustomerController::class)
+    ->group(function () {
+        Route::get('/customer', 'getAll')->name('customers.getAll');
+        Route::post('/customer', 'store')->name('customers.store');
+        Route::put('/customer/{customer}', 'update')->name('customers.update');
+        Route::delete('/customer/{customer}', 'destroy')->name('customers.destroy');
+    });
+
+
